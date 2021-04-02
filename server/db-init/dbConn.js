395341fa6709f9734838c50ee91d4ba42814c3d6
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-04-02 19:20:48 +0530

    connection to db

--- server/db-init/dbConn.js
+++ server/db-init/dbConn.js
@@ -1,16 +1,16 @@
-// var options = {
-//   error: (error, e) => {
-//     if (e.cn) {
-//       // A connection-related error;
-//       console.log("CN:", e.cn);
-//       console.log("EVENT:", error.message);
-//     }
-//   },
-// };
-// const pg = require("pg-promise")(options);
+var options = {
+  error: (error, e) => {
+    if (e.cn) {
+      // A connection-related error;
+      console.log("CN:", e.cn);
+      console.log("EVENT:", error.message);
+    }
+  },
+};
+const pg = require("pg-promise")(options);
 // const config = require("config");
-// const postgresURL = config.get("postgresURL");
-// console.log(config.get("jwtPrivateKey"));
-// const db = pg(postgresURL);
+const postgresURL = "postgres://localhost:5432/igniotron";
+console.log(config.get("jwtPrivateKey"));
+const db = pg(postgresURL);
 
-// module.exports = db;
+module.exports = db;
